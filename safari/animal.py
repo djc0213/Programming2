11f000305a68a38f6142a8bd1a84870fdcade5e5
junghyun <mattew423@gmail.com>
2023-05-17 17:39:07 +0900

    main worked

--- safari/animal.py
+++ safari/animal.py
@@ -5,6 +5,8 @@ class Manager:
         self.world_size = world_size
         self.num_zebras = num_zebras
         self.num_lions = num_lions
+        self.zebras = []
+        self.lions = []
 
     def generate_grid(self):
         grid = [[' '] * self.world_size for _ in range(self.world_size)]
@@ -18,7 +20,7 @@ class Manager:
             while grid[x][y] != ' ':
                 x, y = random.randint(0, self.world_size - 1), random.randint(0, self.world_size - 1)
             grid[x][y] = zebra
-        
+            self.zebras.append(Zebra(x,y))
         # Assign lions to the grid
         lion_count = min(self.num_lions, self.world_size * self.world_size - zebra_count)
         lion_symbols = random.sample(['L'] * self.num_lions, lion_count)
@@ -28,14 +30,21 @@ class Manager:
             while grid[x][y] != ' ':
                 x, y = random.randint(0, self.world_size - 1), random.randint(0, self.world_size - 1)
             grid[x][y] = lion
-        
+            self.lions.append(Lion(x,y))
         return grid
 
     def display_grid(self, grid):
         max_width = max(len(cell) for row in grid for cell in row)
-        
         # Create the horizontal line
         horizontal_line = "+" + "+".join(["-" * (max_width + 2)] * len(grid[0])) + "+"
+        # Print column coordinates at the top
+        print(' ', end='')
+        for i in range(1, self.world_size + 1):
+            if i < 10:
+                print(f' {i}', end='  ')
+            elif i >= 10:
+                print(f' {i}', end=' ')
+        print()
 
         print(horizontal_line)
         for row in grid:
@@ -44,58 +53,35 @@ class Manager:
             print(horizontal_line)
 
 
-# Define the grid size
-#GRID_SIZE = 20
-
-# Create a Manager instance with initial zebra count of 100 and initial lion count of 5
-#manager = Manager(GRID_SIZE, 100, 5)
-
-# Generate and display the grid
-#grid = manager.generate_grid()
-#manager.display_grid(grid)
-
-
-# Display the updated grid
-#print("\nAfter moving an animal:\n")
-#manager.display_grid(grid)
-
-
 class Animal:
     def __init__(self, x, y):
         self.x = x
         self.y = y
         self.age = 0
-    
-    def move(self, occupancy_grid):
-        directions = ['left', 'right', 'up', 'down', 'diagonal_up_left', 'diagonal_up_right', 'diagonal_down_left', 'diagonal_down_right']
-        direction = random.choice(directions)
-        
-        new_x, new_y = self.x, self.y
 
-        if direction == 'left':
-            new_y -= 1
-        elif direction == 'right':
-            new_y += 1
-        elif direction == 'up':
-            new_x -= 1
-        elif direction == 'down':
-            new_x += 1
-        elif direction == 'diagonal_up_left':
-            new_x -= 1
-            new_y -= 1
-        elif direction == 'diagonal_up_right':
-            new_x -= 1
-            new_y += 1
-        elif direction == 'diagonal_down_left':
-            new_x += 1
-            new_y -= 1
-        elif direction == 'diagonal_down_right':
-            new_x += 1
-            new_y += 1
-
-        if 0 <= new_x < len(occupancy_grid) and 0 <= new_y < len(occupancy_grid[0]):
+    def move(self, grid):
+        empty_neighbors = self.get_empty_neighbors(grid)
+        if empty_neighbors:
+            new_coordinate = empty_neighbors[random.randint(0,int(len(empty_neighbors)-1))]
+            new_x = new_coordinate[0]
+            new_y = new_coordinate[1]
+            grid[self.x][self.y] = ' '  # Clear current cell 
             self.x = new_x
             self.y = new_y
+            grid[self.x][self.y] = 'Z' if isinstance(self, Zebra) else 'L'
+
+    def get_empty_neighbors(self, grid):
+        neighbors = []
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (-1, -1), (1, -1), (-1, 1)]  # Right, Left, Down, Up, Diagonals
+        for dx, dy in directions:
+            new_x = self.x + dx
+            new_y = self.y + dy
+            if self.is_valid_position(new_x, new_y, grid) and grid[new_x][new_y] == ' ':
+                neighbors.append((new_x, new_y))
+        return neighbors
+
+    def is_valid_position(self, x, y, grid):
+        return 0 <= x < len(grid) and 0 <= y < len(grid[0])
 
 
     def breed (self, x, y):
@@ -109,3 +95,5 @@ class Zebra(Animal):
 class Lion(Animal):
     def breed(self, x, y):
         print('<<<NOT IMPLEMENTED>>>')
+
+
