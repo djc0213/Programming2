1cae6efbf386807254203cfa4f0d9ee4e53b4881
djc0213 <62947532+djc0213@users.noreply.github.com>
2023-05-31 14:46:09 +0900

    22

--- safari/animal.py
+++ safari/animal.py
@@ -1,99 +1,76 @@
 import random
-
-class Manager:
-    def __init__(self, world_size, num_zebras, num_lions):
-        self.world_size = world_size
-        self.num_zebras = num_zebras
-        self.num_lions = num_lions
-        self.zebras = []
-        self.lions = []
-
-    def generate_grid(self):
-        grid = [[' '] * self.world_size for _ in range(self.world_size)]
-        
-        # Assign zebras to the grid
-        zebra_count = min(self.num_zebras, self.world_size * self.world_size)
-        zebra_symbols = random.sample(['Z'] * self.num_zebras, zebra_count)
-        
-        for zebra in zebra_symbols:
-            x, y = random.randint(0, self.world_size - 1), random.randint(0, self.world_size - 1)
-            while grid[x][y] != ' ':
-                x, y = random.randint(0, self.world_size - 1), random.randint(0, self.world_size - 1)
-            grid[x][y] = zebra
-            self.zebras.append(Zebra(x,y))
-        # Assign lions to the grid
-        lion_count = min(self.num_lions, self.world_size * self.world_size - zebra_count)
-        lion_symbols = random.sample(['L'] * self.num_lions, lion_count)
-        
-        for lion in lion_symbols:
-            x, y = random.randint(0, self.world_size - 1), random.randint(0, self.world_size - 1)
-            while grid[x][y] != ' ':
-                x, y = random.randint(0, self.world_size - 1), random.randint(0, self.world_size - 1)
-            grid[x][y] = lion
-            self.lions.append(Lion(x,y))
-        return grid
-
-    def display_grid(self, grid):
-        max_width = max(len(cell) for row in grid for cell in row)
-        # Create the horizontal line
-        horizontal_line = "+" + "+".join(["-" * (max_width + 2)] * len(grid[0])) + "+"
-        # Print column coordinates at the top
-        print(' ', end='')
-        for i in range(1, self.world_size + 1):
-            if i < 10:
-                print(f' {i}', end='  ')
-            elif i >= 10:
-                print(f' {i}', end=' ')
-        print()
-
-        print(horizontal_line)
-        for row in grid:
-            cells = [cell.center(max_width + 2) for cell in row]
-            print("|" + "|".join(cells) + "|")
-            print(horizontal_line)
-
+from utils import print_TODO
 
 class Animal:
-    def __init__(self, x, y):
+    def __init__(self, y, x):
         self.x = x
         self.y = y
         self.age = 0
+        self.hp = 3
+
+    def move_to(self, grid, target) -> bool:
+        ''' target can be ., L, or Z
+            returns True if moved '''
+        neighbors = self.get_neighbors(grid, target)
+        if len(neighbors) > 0:
+            grid[self.y][self.x] = Empty(self.y, self.x)
+            chosen_neighbor = random.choice(neighbors)
+            self.y, self.x = chosen_neighbor
+            grid[self.y][self.x].hp = 0  # kill
+            grid[self.y][self.x] = self
+            return True
+        else:
+            return False
+
+    def get_neighbors(self, grid, target):
+        ''' target can be ., L, or Z
+            returns a list of coordinates '''
+        world_height = len(grid)
+        world_width = len(grid[0])
+        y, x = self.y, self.x
+        neighbors = []
+        neighbors.append([y - 1, x])
+        neighbors.append([y + 1, x])
+        neighbors.append([y, x - 1])
+        neighbors.append([y, x + 1])
+        neighbors_valid = [neighbor for neighbor in neighbors
+                           if neighbor[0] >= 0
+                           and neighbor[0] < world_height
+                           and neighbor[1] >= 0
+                           and neighbor[1] < world_width
+                           and str(grid[neighbor[0]][neighbor[1]]) == target]
+        return neighbors_valid
+
+    def breed(self, grid):
+        child = self.__class__(self.y, self.x)
+        child.move_to(grid, target='.')
+        grid[self.y][self.x] = self
+
+class Empty(Animal):
+    def __str__(self):
+        return '.'
+
+class Zebra(Animal):
+    def __str__(self):
+        return 'Z'
 
     def move(self, grid):
-        empty_neighbors = self.get_empty_neighbors(grid)
-        if empty_neighbors:
-            new_coordinate = empty_neighbors[random.randint(0,int(len(empty_neighbors)-1))]
-            new_x = new_coordinate[0]
-            new_y = new_coordinate[1]
-            grid[self.x][self.y] = ' '  # Clear current cell 
-            self.x = new_x
-            self.y = new_y
-            grid[self.x][self.y] = 'Z' if isinstance(self, Zebra) else 'L'
+        self.move_to(grid, target='.')
 
-    def get_empty_neighbors(self, grid):
-        neighbors = []
-        directions = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (-1, -1), (1, -1), (-1, 1)]  # Right, Left, Down, Up, Diagonals
-        for dx, dy in directions:
-            new_x = self.x + dx
-            new_y = self.y + dy
-            if self.is_valid_position(new_x, new_y, grid) and grid[new_x][new_y] == ' ':
-                neighbors.append((new_x, new_y))
-        return neighbors
-
-    def is_valid_position(self, x, y, grid):
-        return 0 <= x < len(grid) and 0 <= y < len(grid[0])
-
-
-    def breed (self, x, y):
-        return Animal(x, y)
-    
-class Zebra(Animal):
-    def breed(self, x, y):
-        print('<<<NOT IMPLEMENTED>>>')
-
+    def is_ready_to_breed(self):
+        return self.age != 0 and self.age % 3 == 0
 
 class Lion(Animal):
-    def breed(self, x, y):
-        print('<<<NOT IMPLEMENTED>>>')
+    def __str__(self):
+        return 'L'
 
+    def move(self, grid):
+        hunt_is_successful = self.move_to(grid, target='Z')
+        if hunt_is_successful:
+            self.hp = 3
+        else:
+            self.move_to(grid, target='.')
+            self.hp -= 1
 
+    def is_ready_to_breed(self):
+        return self.age != 0 and self.age % 8 == 0
--- safari/circle_of_life.py
+++ safari/circle_of_life.py
@@ -1,52 +1,83 @@
-from animal import Animal, Manager, Zebra, Lion
+import os 
 import random
-import os
-
-def print_TODO(todo):
-    print(f'<<<NOT IMPLEMENTED: {todo} >>>')
+from animal import Empty, Zebra, Lion
+from utils import print_TODO
 
 class CircleOfLife:
     def __init__(self, world_size, num_zebras, num_lions):
-        self.occupancy = [[False for _ in range(world_size)]
-                          for _ in range(world_size)]
-        print_TODO('get random empty coordinates')
-        self.num_zebras = num_zebras
-        self.num_lions = num_lions
         self.world_size = world_size
-        self.zebras = [Zebra(0, 0) for _ in range(num_zebras)]
-        self.lions = [Lion(0, 0) for _ in range(num_lions)]
+        self.grid = [[Empty(y, x) for y in range(self.world_size)]
+                                  for x in range(self.world_size)]
+        zebra_coords, lion_coords = self.get_random_coords(num_zebras, num_lions)
+        for y, x in zebra_coords:
+            self.grid[y][x] = Zebra(y, x) 
+        for y, x in lion_coords:
+            self.grid[y][x] = Lion(y, x)
         self.timestep = 0
         print('Welcome to AIE Safari!')
         print(f'\tworld size = {world_size}')
-        print(f'\tnumber of zebras = {len(self.zebras)}')
-        print(f'\tnumber of lions = {len(self.lions)}')
-        self.manager = Manager(world_size=self.world_size, num_zebras=self.num_zebras, num_lions=self.num_lions)
-        self.grid = self.manager.generate_grid()
+        print(f'\tnumber of zebras = {num_zebras}')
+        print(f'\tnumber of lions = {num_lions}')
+
+    def get_random_coords(self, num_zebras, num_lions):
+        all_coords = [(y, x) for y in range(self.world_size)
+                       for x in range(self.world_size)]
+        zebra_coords = random.sample(all_coords, num_zebras)
+        all_coords = list(set(all_coords) - set(zebra_coords))
+        lion_coords = random.sample(all_coords, num_lions)
+        return zebra_coords, lion_coords
 
     def display(self):
-        os.system('cls')
+        if os.name == 'nt':
+            os.system('cls')
+        else:
+            os.system('clear')
         print(f'Clock: {self.timestep}')
-        self.manager.display_grid(self.grid)
+        top_coord_str = ' '.join([f'{coord}' for coord in range(len(self.grid))])
+        print('   ' + top_coord_str)
+        for row, line in enumerate(self.grid):
+            buffer = [str(animal) for animal in line]
+            print(f'{row:2} ' + ' '.join(buffer))
         key = input('enter [q] to quit:')
         if key == 'q':
             exit()
 
-    def move_animals(self):
-        for zebra in self.manager.zebras:
-            zebra.move(self.grid)
-        for lion in self.manager.lions:
-            lion.move(self.grid)
+    def step_move(self):
+        animals = [animal for line in self.grid for animal in line
+                   if not isinstance(animal, Empty)]
+        for animal in animals:
+            if animal.hp != 0:
+                animal.move(self.grid)
 
+    def step_breed(self):
+        animals = [animal for line in self.grid for animal in line
+                   if not isinstance(animal, Empty)
+                   and animal.is_ready_to_breed()]
+        for animal in animals:
+            animal.breed(self.grid)
+
+    def housekeeping(self):
+        for y, line in enumerate(self.grid):
+            for x, animal in enumerate(line):
+                if animal.hp == 0:
+                    self.grid[y][x] = Empty(y, x)
+                else:
+                    self.grid[y][x].age += 1
 
     def run(self, num_timesteps=100):
-        os.system('cls')
         self.display()
         for _ in range(num_timesteps):
             self.timestep += 1
-            self.move_animals()
+            self.step_move()
             self.display()
- 
+            self.step_breed()
+            self.display()
+            self.housekeeping()
+
 
 if __name__ == '__main__':
-    safari = CircleOfLife(20, 10, 2)
-    safari.run(1000)
+    safari = CircleOfLife(5, 5, 2)
+    # safari.display()
+    # safari.step_move()
+    # safari.step_breed()
+    safari.run(10)
